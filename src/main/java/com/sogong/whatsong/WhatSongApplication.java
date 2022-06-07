a76b8108850bb44522321e5fb9d381bf62499678
정지우 <jiwoourty@gmail.com>
2022-06-07 15:12:31 +0900

    project init

---
+++ src/main/java/com/sogong/whatsong/WhatSongApplication.java
@@ -0,0 +1,13 @@
+package com.sogong.whatsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatSongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatSongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sogong/whatsong/WhatSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogong.whatsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatSongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
