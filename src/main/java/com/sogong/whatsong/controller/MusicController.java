2399bdf2979a84aacac2b813a51430f48ffb4d7e
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-11 13:18:31 +0900

    #5 implement get music list by genre

--- src/main/java/com/sogong/whatsong/controller/MusicController.java
+++ src/main/java/com/sogong/whatsong/controller/MusicController.java
@@ -5,6 +5,7 @@ import com.sogong.whatsong.controller.dto.response.MusicListResponse;
 import com.sogong.whatsong.service.music.MusicService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class MusicController {
     @GetMapping("/top10")
     public MusicListResponse top10() {
         return musicService.getTop10Music();
+    }
+
+    @GetMapping("/{genre-id}")
+    public MusicListResponse musicList(@PathVariable("genre-id") Long genreId) {
+        return musicService.getMusicByGenre(genreId);
     }
 }
