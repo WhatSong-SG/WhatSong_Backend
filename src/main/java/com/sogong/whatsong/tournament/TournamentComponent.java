8a2574a01f9dfd2610c2c2e05cc3df14426a3d52
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-11 23:47:46 +0900

    #11 tournament component doamin

---
+++ src/main/java/com/sogong/whatsong/tournament/TournamentComponent.java
@@ -0,0 +1,32 @@
+package com.sogong.whatsong.tournament;
+
+import com.sogong.whatsong.entity.music.Music;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class TournamentComponent {
+
+    private Boolean isFinish;
+
+    @Setter
+    private Music music1;
+
+    @Setter
+    private Music music2;
+
+    private Music winner;
+
+    private final Integer round;
+
+    @Setter
+    private TournamentComponent music1PreComponent;
+
+    @Setter
+    private TournamentComponent music2PreComponent;
+
+    public TournamentComponent(Integer round) {
+        this.isFinish = false;
+        this.round = round;
+    }
+}
