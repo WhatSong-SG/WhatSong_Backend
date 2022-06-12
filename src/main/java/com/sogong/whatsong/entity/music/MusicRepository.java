bb206dddcb3bc73ca727850d500602ea7aa8d91d
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-12 13:45:11 +0900

    #11 add native query method
    random find

--- src/main/java/com/sogong/whatsong/entity/music/MusicRepository.java
+++ src/main/java/com/sogong/whatsong/entity/music/MusicRepository.java
@@ -1,13 +1,18 @@
 package com.sogong.whatsong.entity.music;
 
 import com.sogong.whatsong.entity.genre.Genre;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface MusicRepository extends CrudRepository<Music, Long> {
+
+    @Query(value = "select * from music order by rand() limit :limit", nativeQuery = true)
+    List<Music> findOrderByRandom(@Param("limit") Integer limit);
 
     List<Music> findTop10ByOrderByUpDesc();
     List<Music> findByGenreOrderByUpDesc(Genre genre);
