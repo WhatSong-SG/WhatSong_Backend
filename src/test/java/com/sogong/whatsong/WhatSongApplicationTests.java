0f55ea25985d6310d9db571e6dc2927dad8d7c82
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-06-12 22:34:22 +0900

    fix build error

--- src/test/java/com/sogong/whatsong/WhatSongApplicationTests.java
+++ src/test/java/com/sogong/whatsong/WhatSongApplicationTests.java
@@ -3,10 +3,8 @@ package com.sogong.whatsong;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class WhatSongApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
